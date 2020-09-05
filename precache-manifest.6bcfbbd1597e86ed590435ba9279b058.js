1142e987b2f1e5cfa4e100c3522b1428779a97ae
jideybash <jideybash@gmail.com>
2020-09-05 12:55:06 -0500

    Updates

---
+++ precache-manifest.6bcfbbd1597e86ed590435ba9279b058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbcf031d9843905bdca35905dcf9e71",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "c16f3aec6097908e1e8e",
+    "url": "/movies-series-info/static/css/main.f82103b3.chunk.css"
+  },
+  {
+    "revision": "aaff93d93c27b7458704",
+    "url": "/movies-series-info/static/js/2.963484a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-info/static/js/2.963484a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16f3aec6097908e1e8e",
+    "url": "/movies-series-info/static/js/main.e98367c7.chunk.js"
+  },
+  {
+    "revision": "5f36b9ced130c4aeb6ce",
+    "url": "/movies-series-info/static/js/runtime-main.c1e7df6f.js"
+  }
+]);
